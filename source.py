8907198ad0c29992ffc328fb57a4f59758579433
FahimFBA <fahimbinamin@gmail.com>
2021-02-10 18:47:28 +0600

    3 modules imported

---
+++ source.py
@@ -0,0 +1,15 @@
+# start importing some modules
+
+# importing OpenCV
+import cv2
+# using this module , we can process images and videos to identify objects, faces, or even handwriting of a human.
+
+# importing NumPy
+import numpy as np
+# NumPy is usually imported under the np alias. NumPy is a Python library used for working with arrays. It also has functions for working in domain of linear algebra, fourier transform, and matrices
+
+# importing another essential module named time
+import time
+# The Python time module provides many ways of representing time in code, such as objects, numbers, and strings. It also provides functionality other than representing time, like waiting during code execution and measuring the efficiency of our code.
+
+
